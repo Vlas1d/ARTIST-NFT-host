d1144855bbe4a87002b54cda69c9eb275f6dc070
Vlas1d <114921256+Vlas1d@users.noreply.github.com>
2023-04-19 19:16:18 +0300

    Add files via upload

--- js/forum.js
+++ js/forum.js
@@ -16,12 +16,26 @@ const forum_slider = new Swiper('.forum-slider', {
 
 //--------------------------------------------------------------
 
-const questions = document.querySelector('.questions');
-
 const answersBox = document.querySelectorAll('.quest__answers');
 const myAnswerBox = document.querySelectorAll('.quest__my-answer')
 const answersButtons = document.querySelectorAll('.quest__buttons');
 
-questions.addEventListener('click', function () {
 
+answersButtons.forEach((element, index) => {
+    element.querySelector('.button').addEventListener('click', () => {
+        if (element.previousElementSibling.classList.contains('show')) {
+            element.previousElementSibling.classList.remove('show');
+            element.querySelector('.button > span').innerText = 'Show answers (23)';
+        } else {
+            element.previousElementSibling.classList.add('show');
+            element.querySelector('.button > span').innerText = 'Hide answers (23)';
+        }
+    });
+    element.querySelector('button:not(.button)').addEventListener('click', () => {
+        if (element.nextElementSibling.classList.contains('show')) {
+            element.nextElementSibling.classList.remove('show');
+        } else {
+            element.nextElementSibling.classList.add('show');
+        }
+    });
 });
